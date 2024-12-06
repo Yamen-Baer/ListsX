a8d9cd146597ef0c3e80a880fca27b3926f4f437
Yamen Baer <yamen.bayer.main@gmail.com>
2024-12-06 11:45:04 +0400

    03

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import random
 class ListApp:
     def __init__(self, root):
         self.root = root
-        self.root.title("List Manager - Futuristic UI")
+        self.root.title("ListsX - List Manager")
         self.root.geometry("400x600")
         self.root.resizable(False, False)
 
